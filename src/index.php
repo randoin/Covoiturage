0a4aec38e882996bf0d6ed90302c16422b1ed73d
Alekshar <alekshar.shadow@gmail.com>
2014-01-13 11:28:41 +0100

    ajout déconexion

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 	<body>
 		<h1>Site de Covoiturage</h1>
 		<h2></h2>
-		
+		<?php if(isset($_POST['deco'])){session_destroy(); header("location:index.php");}?>
 		
 		<?php if(isset($_SESSION['id'])) echo "<!--";?>
 			<fieldset>
@@ -12,8 +12,10 @@
 			</fieldset>
 		<?php if(isset($_SESSION['id'])) echo "-->"; else echo "<!--"?>
 		
-			<p>Vous êtes connectés</p>
-			
+			<p>Vous êtes connecté</p>
+			<form action='index.php' method='post'>
+				<input type="submit" name="deco" value="se déconnecter">
+			</form>
 		<?php if(!isset($_SESSION['id'])) echo "-->"; ?>
 			
 			
