1a067f29c2183e821a920b932cb2a68b66f1b6fb
battueur <adrien.demoget@gmail.com>
2014-01-13 15:19:38 +0100

    MàJ

--- base.sql
+++ base.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Lun 13 Janvier 2014 à 14:23
+-- Généré le : Lun 13 Janvier 2014 à 15:17
 -- Version du serveur: 5.1.63
 -- Version de PHP: 5.3.3-7+squeeze15
 
@@ -24,16 +24,16 @@ USE `projet_agileb`;
 -- --------------------------------------------------------
 
 --
--- Structure de la table `COVOITURAGE`
+-- Structure de la table `covoiturage`
 --
 
-CREATE TABLE IF NOT EXISTS `COVOITURAGE` (
-  `NUMEROCOVOIT` char(32) COLLATE utf8_unicode_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `covoiturage` (
+  `NUMEROCOVOIT` int(100) NOT NULL,
   `MONTANT` int(100) NOT NULL,
   `PLAQUEIMMATRICULATION` char(32) COLLATE utf8_unicode_ci NOT NULL,
   `IDENTIFIANTLIEU` char(32) COLLATE utf8_unicode_ci NOT NULL,
   `IDENTIFIANTLIEU_ARRIVEE` char(32) COLLATE utf8_unicode_ci NOT NULL,
-  `PLACESDISPO` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `PLACESDISPO` int(100) DEFAULT NULL,
   `DATEDEPART` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`NUMEROCOVOIT`),
   KEY `I_FK_COVOITURAGE_VEHICULE` (`PLAQUEIMMATRICULATION`),
@@ -42,96 +42,136 @@ CREATE TABLE IF NOT EXISTS `COVOITURAGE` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `COVOITURAGE`
+-- Contenu de la table `covoiturage`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `ETREPASSAGER`
+-- Structure de la table `demandercovoiturage`
 --
 
-CREATE TABLE IF NOT EXISTS `ETREPASSAGER` (
+CREATE TABLE IF NOT EXISTS `demandercovoiturage` (
   `NUMEROCOVOIT` char(32) COLLATE utf8_unicode_ci NOT NULL,
   `MATRICULE` char(32) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`NUMEROCOVOIT`,`MATRICULE`),
+  KEY `I_FK_ETREPASSAGER_COVOITURAGE` (`NUMEROCOVOIT`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `demandercovoiturage`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etape`
+--
+
+CREATE TABLE IF NOT EXISTS `etape` (
+  `NUMEROCOVOIT` int(100) NOT NULL,
+  `IDENTIFIANTLIEU` char(32) COLLATE utf8_unicode_ci NOT NULL,
+  `NUMEROETAPE` int(100) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`NUMEROCOVOIT`,`IDENTIFIANTLIEU`,`NUMEROETAPE`),
+  KEY `I_FK_PASSERPAR_COVOITURAGE` (`NUMEROCOVOIT`),
+  KEY `I_FK_PASSERPAR_LIEU` (`IDENTIFIANTLIEU`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `etape`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etrepassager`
+--
+
+CREATE TABLE IF NOT EXISTS `etrepassager` (
+  `NUMEROCOVOIT` char(32) COLLATE utf8_unicode_ci NOT NULL,
+  `MATRICULE` char(32) COLLATE utf8_unicode_ci NOT NULL,
+  `CODEPASSAGER` char(10) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`NUMEROCOVOIT`,`MATRICULE`),
   KEY `I_FK_ETREPASSAGER_COVOITURAGE` (`NUMEROCOVOIT`),
   KEY `I_FK_ETREPASSAGER_UTILISATEUR` (`MATRICULE`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `ETREPASSAGER`
+-- Contenu de la table `etrepassager`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `LIEU`
+-- Structure de la table `generercode`
 --
 
-CREATE TABLE IF NOT EXISTS `LIEU` (
-  `IDENTIFIANTLIEU` char(32) COLLATE utf8_unicode_ci NOT NULL,
+CREATE TABLE IF NOT EXISTS `generercode` (
+  `CODEPAIEMENT` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `MATRICULECONDUCTEUR` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `MATRICULEPASSAGER` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `NUMEROCOVOIT` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`MATRICULECONDUCTEUR`,`MATRICULEPASSAGER`,`NUMEROCOVOIT`),
+  UNIQUE KEY `CODEPAIEMENT` (`CODEPAIEMENT`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `generercode`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `lieu`
+--
+
+CREATE TABLE IF NOT EXISTS `lieu` (
+  `IDENTIFIANTLIEU` int(10) NOT NULL,
   `NOMVILLE` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`IDENTIFIANTLIEU`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `LIEU`
+-- Contenu de la table `lieu`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `PASSERPAR`
+-- Structure de la table `utilisateur`
 --
 
-CREATE TABLE IF NOT EXISTS `PASSERPAR` (
-  `NUMEROCOVOIT` int(100) NOT NULL,
-  `IDENTIFIANTLIEU` char(32) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`NUMEROCOVOIT`,`IDENTIFIANTLIEU`),
-  KEY `I_FK_PASSERPAR_COVOITURAGE` (`NUMEROCOVOIT`),
-  KEY `I_FK_PASSERPAR_LIEU` (`IDENTIFIANTLIEU`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Contenu de la table `PASSERPAR`
---
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `UTILISATEUR`
---
-
-CREATE TABLE IF NOT EXISTS `UTILISATEUR` (
+CREATE TABLE IF NOT EXISTS `utilisateur` (
   `MATRICULE` int(100) NOT NULL,
   `NOM` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   `PRENOM` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   `LOGIN` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   `MOTDEPASSE` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ADRESSEMAIL` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `NUMEROTEL` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `NUMEROTEL` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`MATRICULE`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `UTILISATEUR`
+-- Contenu de la table `utilisateur`
 --
 
-INSERT INTO `UTILISATEUR` (`MATRICULE`, `NOM`, `PRENOM`, `LOGIN`, `MOTDEPASSE`, `ADRESSEMAIL`, `NUMEROTEL`) VALUES
+INSERT INTO `utilisateur` (`MATRICULE`, `NOM`, `PRENOM`, `LOGIN`, `MOTDEPASSE`, `ADRESSEMAIL`, `NUMEROTEL`) VALUES
 (0, 'lucas', 'boutrot', 'thornydre', 'john', 'thornydre@gmail.com', '0625478935');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `VEHICULE`
+-- Structure de la table `vehicule`
 --
 
-CREATE TABLE IF NOT EXISTS `VEHICULE` (
+CREATE TABLE IF NOT EXISTS `vehicule` (
   `PLAQUEIMMATRICULATION` char(32) COLLATE utf8_unicode_ci NOT NULL,
   `MATRICULE` char(32) COLLATE utf8_unicode_ci NOT NULL,
   `MODELE` char(32) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -143,6 +183,6 @@ CREATE TABLE IF NOT EXISTS `VEHICULE` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Contenu de la table `VEHICULE`
+-- Contenu de la table `vehicule`
 --
 
