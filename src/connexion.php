829e278f4b3764557ffd912f020e214c972b61a6
Jokerzz <louis.farin@gmail>
2014-01-13 10:26:09 +0100

    Page connexion

---
+++ src/connexion.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<form action='traitement_connexion.php' method='post'>";
+echo "<p><fieldset> <h1>Connexion covoiturage</h1></p>";
+echo "<p>Login : <input type='text' name='ident'/></p>";
+echo "<p>Mot de passe : <input type='password' name='pass'/></p>";
+echo "<p><input type='submit' value='valider'/></p>"
+
+?>
