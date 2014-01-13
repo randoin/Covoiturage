c1475c1b66342844af10ed778924794c36513e2a
Alekshar <alekshar.shadow@gmail.com>
2014-01-13 10:58:04 +0100

    Ajout de pages (anticipation)

---
+++ src/rechercher.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Covoiturage</title>
+	</head>
+	<body>
+		<h1>Site de Covoiturage</h1>
+		<h2></h2>
+		<a href="demander.php">Demander à participer à un covoiturage</a><br>
+		<a href="inscriptionCovoiturage.php">S'inscrire à un covoiturage</a><br>
+	</body>
+	<footer>
+		<h5>A propos</h5>
+	</footer>
+</html>
