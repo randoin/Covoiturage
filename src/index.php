c5d7a072f12f94676ec694bf45f64e65f0e6ddad
Alekshar <alekshar.shadow@gmail.com>
2014-01-13 10:30:16 +0100

    Ajout de l'index (acceuil)

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Covoiturage</title>
+	</head>
+	<body>
+		<h1>Site de Covoiturage</h1>
+		<h2></h2>
+		
+		<fieldset>
+			<legend>Connexion</legend>
+			<a href="#">Se connecter</a><br>
+			<a href="#">S'inscrire</a><br>
+		</fieldset>
+	</body>
+	<footer>
+	<h5>A propos</h5>
+	</footer>
+</html>
