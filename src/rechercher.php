5dce8a0c053921487d2c653712b2802c35864494
Alekshar <alekshar.shadow@gmail.com>
2014-01-13 15:48:05 +0100

    mise a jour style.css et page d'acceuil

--- src/rechercher.php
+++ src/rechercher.php
@@ -1,6 +1,6 @@
 <?php include "header.php" ?>
-		<h1>Site de Covoiturage</h1>
-		<h2></h2>
+		<fieldset>
 		<a href="demander.php">Demander à participer à un covoiturage</a><br>
 		<a href="inscriptionCovoiturage.php">S'inscrire à un covoiturage</a><br>
+		</fieldset>
 <?php include "footer.php" ?>
