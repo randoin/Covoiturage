9b2ebdfdeab74a2b3e423541de5830132f51bd39
Alekshar <alekshar.shadow@gmail.com>
2014-01-13 16:16:34 +0100

    Suppression des fichiers inutiles

--- src/rechercher.php
+++
@@ -1,6 +0,0 @@
-<?php include "header.php" ?>
-		<fieldset>
-		<a href="demander.php">Demander à participer à un covoiturage</a><br>
-		<a href="inscriptionCovoiturage.php">S'inscrire à un covoiturage</a><br>
-		</fieldset>
-<?php include "footer.php" ?>
