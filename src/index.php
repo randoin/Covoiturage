8a98c0212e3f57cc100571b50a8216998488c659
aniwey <aniwey@gmail.com>
2014-01-14 10:37:51 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -25,15 +25,15 @@
 				}
 			?>
 			</p>
-			<b>Si vous êtes passager :</b>
+			<b>Si vous êtes passager :</b><br/>
 			<a href="recherche.php"><input type="button" value="Rechercher un covoiturage"></a><br/><br/>
-			<b>Si vous êtes conducteur :</b>
+			<b>Si vous êtes conducteur :</b><br/>
 			<a href="proposition.php"><input type="button" value="Proposer un covoiturage"></a><br/>
 			<a href="valider.php"><input type="button" value="Valider une demande de covoiturage"></a><br/>
 			<a href="declarerCode.php"><input type="button" value="Entrer les codes passagers de vos covoitureurs pour être payé"></a><br><br/>	
 			<b>Si vous avez terminé d'utiliser le site :</b><br/>
 			<form action='index.php' method='post'>
-				<input type="submit" name="deco" value="se déconnecter">
+				<input type="submit" name="deco" value="Se déconnecter">
 			</form>
 			</fieldset>
 		<?php if(!isset($_SESSION['id'])) echo "-->"; ?>
