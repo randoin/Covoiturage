62b4556f075d89e78cf0038873d21f0c1a98f15b
Alekshar <alekshar.shadow@gmail.com>
2014-01-14 09:42:26 +0100

    Valider en fonction de mysql_connect

--- src/valider.php
+++ src/valider.php
@@ -1,15 +1,11 @@
-<?php session_start(); include "header.php"; ?>
+<?php session_start(); include "header.php"; include "base.php"; ?>
 
 <?php
 	if(isset($_POST['a'])){
-		$db = new PDO('mysql:host=localhost;dbname=projet_agileb', 'root', '');
-		$db->query("UPDATE  `projet_agileb`.`demandercovoiturage` SET  `VALIDE` =  '1' WHERE  `demandercovoiturage`.`NUMEROCOVOIT` =".$_POST['covoit']." AND `demandercovoiturage`.`MATRICULE` =".$_POST['demande']);
-		echo "accepter".$_POST['covoit'].$_POST['demande'];
+		mysql_query("UPDATE  `projet_agileb`.`demandercovoiturage` SET  `VALIDE` =  '1' WHERE  `demandercovoiturage`.`NUMEROCOVOIT` =".$_POST['covoit']." AND `demandercovoiturage`.`MATRICULE` =".$_POST['demande']);
 	}
 	else if(isset($_POST['r'])){
-		$db = new PDO('mysql:host=localhost;dbname=projet_agileb', 'root', '');
-		$db->query("DELETE FROM `projet_agileb`.`demandercovoiturage` WHERE `demandercovoiturage`.`NUMEROCOVOIT` = ".$_POST['covoit']." AND `demandercovoiturage`.`MATRICULE` = ".$_POST['demande']);
-		echo "refuser".$_POST['covoit'].$_POST['demande'];
+		mysql_query("DELETE FROM `projet_agileb`.`demandercovoiturage` WHERE `demandercovoiturage`.`NUMEROCOVOIT` = ".$_POST['covoit']." AND `demandercovoiturage`.`MATRICULE` = ".$_POST['demande']);
 	}
 ?>
 
@@ -29,13 +25,12 @@
 		<table>
 			<tr><td>Covoiturage</td><td>Demandeur</td><td>@mail</td><td></td><td></td></tr>
 			<?php
-				$db = new PDO('mysql:host=localhost;dbname=projet_agileb', 'root', '');
-				$req = $db->query('SELECT `IDENTIFIANTLIEU`,`IDENTIFIANTLIEU_ARRIVEE`,`DATEDEPART`,`NOM`,`PRENOM`,`ADRESSEMAIL`,NUMEROCOVOIT,`MATRICULE` FROM `covoiturage` 
+				$req = mysql_query('SELECT `IDENTIFIANTLIEU`,`IDENTIFIANTLIEU_ARRIVEE`,`DATEDEPART`,`NOM`,`PRENOM`,`ADRESSEMAIL`,NUMEROCOVOIT,`MATRICULE` FROM `covoiturage` 
 				JOIN `demandercovoiturage` using(`NUMEROCOVOIT`) 
 				JOIN `utilisateur` using(`MATRICULE`) 
 				WHERE `CONDUCTEUR` = (select `MATRICULE` from utilisateur where `ADRESSEMAIL` = \''.$_SESSION['id'].'\') and valide = 0');
 				if(!empty($req))
-					foreach($req as $row){
+					while ($row = mysql_fetch_array($req)){
 						echo "<tr><td>de ".$row['IDENTIFIANTLIEU']." a ".$row['IDENTIFIANTLIEU_ARRIVEE']." le ".$row['DATEDEPART']."</td><td>".
 						$row['NOM']." ".$row['PRENOM']."</td><td>".$row['ADRESSEMAIL'].'</td><td><input type="submit" value="accepter" name="a" onclick="set('.$row['NUMEROCOVOIT'].','.$row['MATRICULE'].');"></td><td><input type="submit" value="refuser" name="r" onclick="set('.$row['NUMEROCOVOIT'].','.$row['MATRICULE'].');">
 						';
