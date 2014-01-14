9f2c1f6a1b3cd425b868a638d2b3f5c14d8b13b4
aniwey <aniwey@gmail.com>
2014-01-14 12:54:40 +0100

    Update About.php

--- src/About.php
+++ src/About.php
@@ -1,7 +1,7 @@
 ﻿<?php include("header.php"); ?>	
 <meta charset="UTF-8">
-<fieldset>
 <h1>A propos </h1>
+<fieldset>
 Ce projet est réalisé dans le cadre de nos études à l'IUT de Caen, antenne d'Ifs, dans le cadre de notre DUT.<br/>
 Il a pour but de nous apprendre à réaliser un projet de manière agile au sein d'un groupe de travail.
 </fieldset>
