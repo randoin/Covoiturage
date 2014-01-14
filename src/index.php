abe27792b6962809ae91f8011eda1ba196da9fa9
aniwey <aniwey@gmail.com>
2014-01-14 10:36:52 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -26,12 +26,12 @@
 			?>
 			</p>
 			<b>Si vous êtes passager :</b>
-			<a href="recherche.php"><input type="button" value="Rechercher un covoiturage"></a><br>
+			<a href="recherche.php"><input type="button" value="Rechercher un covoiturage"></a><br/><br/>
 			<b>Si vous êtes conducteur :</b>
-			<a href="proposition.php"><input type="button" value="Proposer un covoiturage"></a><br>
-			<a href="valider.php"><input type="button" value="Valider une demande de covoiturage"></a><br>
-			<a href="declarerCode.php"><input type="button" value="Entrer les codes passagers de vos covoitureurs pour être payé"></a><br>	
-			<b>Si vous avez terminé d'utiliser le site :</b>
+			<a href="proposition.php"><input type="button" value="Proposer un covoiturage"></a><br/>
+			<a href="valider.php"><input type="button" value="Valider une demande de covoiturage"></a><br/>
+			<a href="declarerCode.php"><input type="button" value="Entrer les codes passagers de vos covoitureurs pour être payé"></a><br><br/>	
+			<b>Si vous avez terminé d'utiliser le site :</b><br/>
 			<form action='index.php' method='post'>
 				<input type="submit" name="deco" value="se déconnecter">
 			</form>
