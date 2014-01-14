87134dd865e07e15265d877cde35aea776598cc3
Alekshar <alekshar.shadow@gmail.com>
2014-01-14 13:42:47 +0100

    ajout de la liste des demande en attente

--- src/index.php
+++ src/index.php
@@ -22,6 +22,7 @@
 			</p>
 			<b>Vous êtes passager :</b><br/>
 			<a href="recherche.php"><input type="button" value="Rechercher un covoiturage"></a><br/><br/>
+			<a href="liste_valide.php"><input type="button" value="Voir les demandes de covoiturage"></a><br/><br/>
 			<b>Vous êtes conducteur :</b><br/>
 			<a href="proposition.php"><input type="button" value="Proposer un covoiturage"></a><br/>
 			<a href="valider.php"><input type="button" value="Valider une demande de covoiturage"></a><br/>
---
+++ src/liste_valide.php
@@ -0,0 +1,33 @@
+﻿<?php session_start(); include "header.php"; include "base.php"; ?>
+
+<h1>Etat des demandes de covoiturage</h1>
+<fieldset>
+		<table>
+			<?php
+				$req = mysql_query("SELECT  `DATEDEPART` ,  `HEUREDEPART` ,  `IDENTIFIANTLIEU` ,  `IDENTIFIANTLIEU_ARRIVEE` ,  `VALIDE`  FROM  `demandercovoiturage` 
+					JOIN covoiturage
+					USING (  `NUMEROCOVOIT` ) 
+					WHERE  `MATRICULE` = ( 
+					SELECT  `MATRICULE` 
+					FROM utilisateur
+					WHERE  `ADRESSEMAIL` =  '".$_SESSION['id']."' ) ");
+				if(mysql_num_rows($req) > 0){
+				    echo "<tr><td>Covoiturage</td><td>Etat de la demande</td></tr>";
+					while ($row = mysql_fetch_array($req)){
+						if($row['VALIDE'] == 1)
+							echo "<tr><td>le ".$row['DATEDEPART']." à ".$row['HEUREDEPART']." de ".$row['IDENTIFIANTLIEU']." à ".$row['IDENTIFIANTLIEU_ARRIVEE']."  </td><td>confirmé<td></tr>";
+						else
+							echo "<tr><td>le ".$row['DATEDEPART']." à ".$row['HEUREDEPART']." de ".$row['IDENTIFIANTLIEU']." à ".$row['IDENTIFIANTLIEU_ARRIVEE']."  </td><td>en attente</td></tr>";
+					}
+				}
+				else
+					echo "<tr><td>Aucune demande en cours. </td></tr>";
+			?>
+		</table>
+	</form>
+	<br/>
+	<a href='index.php'><input type='button' value="Retour a l'accueil"></a>
+</fieldset>
+
+
+<?php include "footer.php"; ?>
