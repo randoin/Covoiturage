df0c8138ba836fca1c3eb6021e70712ca5088d11
thornydre <thornydre@gmail.com>
2014-01-14 13:31:07 +0100

    Correction

--- src/traitement_connexion.php
+++ src/traitement_connexion.php
@@ -19,10 +19,5 @@
 	mysql_free_result ($exec);
 	mysql_close();
 
-	if(!isset($_SESSION["id"])){
-		header("Location:connexion.php");
-	}
-	else{
-		header("Location:index.php");
-	}
+	header("Location:index.php");
  ?>
